535d3363c824ba7c8b4bfaf7142aeec80c3e7fdd
Hemanth Kumar <hemanth7787@gmail.com>
2015-07-24 00:54:43 +0530

    Geolocation and share (intent)

--- app/src/main/java/com/novytes/zion/sunshine/DetailActivityFragment.java
+++ app/src/main/java/com/novytes/zion/sunshine/DetailActivityFragment.java
@@ -3,7 +3,13 @@ package com.novytes.zion.sunshine;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -13,8 +19,12 @@ import android.widget.TextView;
  * A placeholder fragment containing a simple view.
  */
 public class DetailActivityFragment extends Fragment {
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+    private  static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private String mforecastStr;
 
     public DetailActivityFragment() {
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -23,10 +33,34 @@ public class DetailActivityFragment extends Fragment {
         Intent intent = getActivity().getIntent();
         View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
         if(intent!=null && intent.hasExtra(Intent.EXTRA_TEXT)){
-            String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            mforecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
             TextView textView = (TextView) rootView.findViewById(R.id.detail_text);
-            textView.setText(forecastStr);
+            textView.setText(mforecastStr);
         }
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        inflater.inflate(R.menu.detail_fragment, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        ShareActionProvider mShareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        if(mShareActionProvider != null){
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        }else {
+            Log.d(LOG_TAG, "Share action provider is null");
+        }
+
+
+    }
+
+    private Intent createShareForecastIntent(){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mforecastStr + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
     }
 }
